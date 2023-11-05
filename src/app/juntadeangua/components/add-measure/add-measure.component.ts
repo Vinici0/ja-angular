cb18b21d4a76f932c8bf3fc9ba70d4924d4de432
LeonardoB-10 <vinicio.borja10@gmail.com>
2023-11-05 07:55:40 -0500

    Disable form fields and add SweetAlert2 notification.

--- src/app/juntadeangua/components/add-measure/add-measure.component.ts
+++ src/app/juntadeangua/components/add-measure/add-measure.component.ts
@@ -10,6 +10,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { SearchClientComponent } from '../../modals/search-client/search-client.component';
 import { MatDialog } from '@angular/material/dialog';
 import { CustomerService } from '../../services/customer.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-measure',
@@ -20,9 +21,9 @@ export class AddMeasureComponent implements OnInit {
   cliente: any;
 
   public myForm: FormGroup = this.fb.group({
-    nombre: ['', [Validators.required]],
-    ruc: ['', [Validators.required]],
-    telefono: ['', [Validators.required]],
+    nombre: [{ value: '', disabled: true }, [Validators.required]],
+    ruc: [{ value: '', disabled: true }, [Validators.required]],
+    telefono: [{ value: '', disabled: true }, [Validators.required]],
     lote: ['', [Validators.required]],
     manzana: ['', [Validators.required]],
     codigo: ['', [Validators.required]],
@@ -91,6 +92,38 @@ export class AddMeasureComponent implements OnInit {
         }
       });
       console.log(formData);
+    }
+  }
+
+  createMeusereAndUpdateCustomer() {
+    if (this.myForm.valid) {
+      const formData = this.myForm.value;
+      Object.keys(formData).forEach((key) => {
+        if (typeof formData[key] === 'string') {
+          formData[key] = formData[key].toUpperCase();
+          this.myForm.get(key)?.setValue(formData[key]); // Actualizar el valor en el formulario
+        }
+      });
+      console.log(formData);
+      this.customerService
+        .createMeusereAndUpdateCustomer(formData)
+        .subscribe((resp: any) => {
+          console.log(resp);
+          if (resp.ok) {
+            Swal.fire({
+              toast: true,
+              position: 'top-end',
+              showConfirmButton: false,
+              width: 450,
+              timer: 2000,
+              title: 'Se ha agregado correctamente',
+              icon: 'success',
+            });
+            this.router.navigateByUrl(
+              '/junta-de-angua/pages/add-measure?id=' + resp.data.idCliente
+            );
+          }
+        });
     }
   }
 }
